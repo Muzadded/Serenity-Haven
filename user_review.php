5fe7f2cd62c2867a9b70a8ea2a237d8149d73f09
Muzadded Chowdhury <alifchowdhury27@gmail.com>
2024-09-10 11:07:29 +0600

    user_review

---
+++ user_review.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/user_review.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div id="nav">
+        <div id="left_nav">
+            <img src="assets/logo.jpg" style="width: 50px; height: 50px;" alt=""><span style="margin-left:10px ;font-size: 20px; font-weight: bold; color: #399918;">Serenity Haven</span>
+        </div>
+        <div id="right_nav">
+            <div id="right_nav_link">
+                <a class="nav_link" href="index.html" style="color: #399918;">Home</a>
+                <a class="nav_link" href="about_us.html">About Us</a>
+                <a class="nav_link" href="our_services.html">Our Services</a>
+                <a class="nav_link" href="plans.php">Plans & Donate</a>
+                <a class="nav_link" href="plans.php">Dashboard</a>
+            </div>
+            <div id="right_nav_btn">
+                <a class="btn btn-success login" href="login.php" role="button">Log out</a>
+                <a class="btn btn-success contact_btn" href="contact_us.php" role="button">Contact Us</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <!-- Sidebar -->
+        <aside class="sidebar">
+            <ul>
+                <li><a href="user_profile_edit.php">Edit Profile</a></li>
+                <li><a href="user_profile_delete.php" >Delete Membership</a></li>
+                <li><a href="#">Help</a></li>
+                <li><a href="#">Terms & Conditions</a></li>
+                <li><a href="user_review.php" style="color: #399918; font-weight:bold">Review</a></li>
+            </ul>
+        </aside>
+
+        <!-- Profile Edit Form -->
+        <main class="comment-section">
+            <h2>Give your Thoughts About Us</h2>
+            <form action="#" method="POST">
+                <div class="form-group">
+                    <label for="comment">Comment</label>
+                    <textarea id="comment" name="comment" placeholder="Enter your email or comment"></textarea>
+                </div>
+                <div class="button-group">
+                    <button type="button" class="cancel-btn">Cancel</button>
+                    <button type="submit" class="send-btn">Send</button>
+                </div>
+            </form>
+        </main>
+    </div>
+
+    <div id="footer">
+        <div id="footer_left">
+            <img style="width: 50px; height: 50px;" src="assets/logo.jpg" alt=""><span style="margin-left:10px ;font-size: 20px; font-weight: bold; color: #399918;">Serenity Care Center</span>
+            <p style="opacity: 0.8;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium accusantium vel autem ab quos quis iure quisquam aperiam. Sapiente accusantium neque amet, natus ratione sint facere nam sunt quam similique.</p>
+            <table>
+                <tr>
+                    <td><img style="width: 20px; height: 20px;" src="assets/facebook-round-black-icon4588.logowik.com.webp" alt=""></td>
+                    <td><img style="width: 20px; height: 20px;" src="assets/insta.png" alt=""></td>
+                    <td><img style="width: 20px; height: 20px;" src="assets/twitt.png" alt=""></td>
+                    <td><img style="width: 20px; height: 20px;" src="assets/ln.png" alt=""></td>
+                </tr>
+            </table>
+        </div>
+        <div id="footer_right">
+            <table>
+                <tr>
+                    <td>Quick Link</td>
+                    <td>Legal</td>
+                </tr>
+                <tr>
+                    <td>Home</td>
+                    <td>Terms of Service</td>
+                </tr>
+                <tr>
+                    <td>About</td>
+                    <td>Privacy Policy</td>
+                </tr>
+                <tr>
+                    <td>Portfolio</td>
+                </tr>
+                <tr>
+                    <td>Services</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</body>
+
+</html>
